370919565882b7c84b160cc1a63166a457d75205
angelovangel <aangeloo@gmail.com>
2025-09-16 11:14:51 +0300

    fix cleanup-miniserve

--- bin/cleanup-miniserve.sh
+++ bin/cleanup-miniserve.sh
@@ -5,10 +5,9 @@
 
 MINISERVE_DIR="miniserve"
 LOGFILE="$MINISERVE_DIR/miniserve.log"
-NUM="${1:-2}" # Default to 2 days if not specified
-UNITS="${2:-d}" # Default to days if not specified
+DAYS="${1:-2}" # Default to 2 days if not specified
 
-find "$MINISERVE_DIR" -maxdepth 1 -type f -name "*.tar.gz" -mtime +"$NUM""$UNITS" | while read -r file; do
-  echo "$(date '+%Y-%m-%d %H:%M:%S') deleted $file (older than $NUM $UNITS)" >> "$LOGFILE"
+find "$MINISERVE_DIR" -maxdepth 1 -type f -name "*.tar.gz" -mtime +"$DAYS" | while read -r file; do
+  echo "$(date '+%Y-%m-%d %H:%M:%S') deleted $file (older than $DAYS)" >> "$LOGFILE"
   rm -f "$file"
 done
