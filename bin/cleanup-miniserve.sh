06b11f9c4a4affdf6eab6a4d7599477afacd2471
angelovangel <aangeloo@gmail.com>
2025-09-21 12:53:46 +0300

    better miniserve cleanup

--- bin/cleanup-miniserve.sh
+++ bin/cleanup-miniserve.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-# Usage: ./cleanup_miniserve.sh <days>
-# Example: ./cleanup_miniserve.sh 2
+# Usage: ./cleanup_miniserve.sh <dir> <days>
+# Example: ./cleanup_miniserve.sh /home/serve-dir 2
 
-MINISERVE_DIR="miniserve"
+MINISERVE_DIR="$1"
 LOGFILE="logs/miniserve.log"
-DAYS="${1:-14}" # Default to 14 days if not specified
+DAYS="${2:-14}" # Default to 14 days if not specified
 
 find "$MINISERVE_DIR" -maxdepth 1 -type f -name "*.tar.gz" -mtime +"$DAYS" | while read -r file; do
   echo "$(date '+%Y-%m-%d %H:%M:%S') deleted $file (older than $DAYS)" >> "$LOGFILE"
