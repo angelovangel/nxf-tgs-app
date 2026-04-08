1c438f8a40cb86f677fc479cf271776a9a9d2068
angelovangel <aangeloo@gmail.com>
2026-04-08 14:23:36 +0300

    setup backup on dw

--- bin/cleanup-miniserve.sh
+++ bin/cleanup-miniserve.sh
@@ -3,11 +3,15 @@
 # Usage: ./cleanup_miniserve.sh <dir> <days>
 # Example: ./cleanup_miniserve.sh /home/serve-dir 2
 
+source secrets
+
 MINISERVE_DIR="$1"
 LOGFILE="logs/miniserve.log"
 DAYS="${2:-14}" # Default to 14 days if not specified
 
 find "$MINISERVE_DIR" -maxdepth 1 -type f -name "*.tar.gz" -mtime +"$DAYS" | while read -r file; do
-  echo "$(date '+%Y-%m-%d %H:%M:%S') deleted $file (older than $DAYS)" >> "$LOGFILE"
-  rm -f "$file"
+  echo $file;
+  curl --fail -T $file -u $USERNAME:$PASS $URL/$STORAGE/ &&  \
+  echo "$(date '+%Y-%m-%d %H:%M:%S') $file transferred OK" >> "$LOGFILE" || echo "$(date '+%Y-%m-%d %H:%M:%S') $file transfer failed!" >> "$LOGFILE"; 
+  rm -f "$file" && echo "$(date '+%Y-%m-%d %H:%M:%S') $file deleted (older than $DAYS)" >> "$LOGFILE"
 done
