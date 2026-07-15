32ccd08ccf50d7e3766d80a3de2ed92d663fe5e9
angelovangel <aangeloo@gmail.com>
2026-07-15 11:21:50 +0300

    fix cleanup data

--- bin/cleanup-miniserve.sh
+++ bin/cleanup-miniserve.sh
@@ -11,7 +11,10 @@ DAYS="${2:-14}" # Default to 14 days if not specified
 
 find "$MINISERVE_DIR" -maxdepth 1 -type f -name "*.tar.gz" -mtime +"$DAYS" | while read -r file; do
   echo $file;
-  curl --fail -T $file -u $USERNAME:$PASS $URL/$STORAGE/ &&  \
-  echo "$(date '+%Y-%m-%d %H:%M:%S') $file transferred OK" >> "$LOGFILE" || echo "$(date '+%Y-%m-%d %H:%M:%S') $file transfer failed!" >> "$LOGFILE"; 
-  rm -f "$file" && echo "$(date '+%Y-%m-%d %H:%M:%S') $file deleted (older than $DAYS)" >> "$LOGFILE"
+  if curl --fail -T "$file" -u "$USERNAME:$PASS" "$URL/$STORAGE/"; then
+    echo "$(date '+%Y-%m-%d %H:%M:%S') $file transferred OK" >> "$LOGFILE"
+    rm -f "$file" && echo "$(date '+%Y-%m-%d %H:%M:%S') $file deleted (older than $DAYS days)" >> "$LOGFILE"
+  else
+    echo "$(date '+%Y-%m-%d %H:%M:%S') $file transfer failed! File NOT deleted." >> "$LOGFILE"
+  fi
 done
